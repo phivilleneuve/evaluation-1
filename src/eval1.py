d9243bc3d025f41b7bc359516d87da21757e656c
Philippe Villeneuve <philippeqc@gmail.com>
2024-12-25 15:40:39 -0500

    Avec test local qui vont passer

---
+++ src/eval1.py
@@ -0,0 +1,6 @@
+def addition(x, y):
+    return 3
+
+
+if __name__ == "__main__":
+    print(addition(1, 2))
---
+++ tests/test_example.py
@@ -0,0 +1,6 @@
+import eval1
+
+
+# test_example.py
+def test_addition():
+    assert eval1.addition(1, 2) == 3
